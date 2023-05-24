58789f91ecc13f1165a3e23d07c8b6209208efb6
Clawber <jssarenas@up.edu.ph>
2023-05-25 03:03:10 +0800

    refactor: Added sample POST and PUT contents

--- routes/jeeps.js
+++ routes/jeeps.js
@@ -43,9 +43,11 @@ router.get('/', getJeeps)
 router.get('/:id', getJeepById)
 
 
-
-// json data 
-// {"coords": [14.64827247,121.0737752]}
+/*
+Sample request:
+POST website/api/jeeps/1
+{"coords": [14.64827247,121.0737752]}
+*/
 router.post('/:id', (request, response) => {
   const { error } = coords_schema.validate(request.body)
   if (error) {
@@ -57,11 +59,11 @@ router.post('/:id', (request, response) => {
     const data = request.body;
 
     console.log(data);
-    // data = { coords: [ 14.64827247, 121.0737752 ] }
 
     let x = data.coords[0]
     let y = data.coords[1]
 
+    // Update jeep location on the database
     let query = `UPDATE tracker SET coords = '(${x},${y})' WHERE id = ${id}`;
 
     pool.query(query, (error, results) => {
@@ -75,6 +77,14 @@ router.post('/:id', (request, response) => {
 })
 
 
+/*
+Sample request:
+POST website/api/jeeps/1
+{
+  "id": 1,
+  "coords": [14.6575533,121.0742258]
+}
+*/
 router.put('/:id', (req, res) => {
   const jeep = jeeps.find(c => c.id === parseInt(req.params.id))
 
@@ -82,27 +92,9 @@ router.put('/:id', (req, res) => {
     res.status(404).send('The jeep with the given ID was not found')
   }
 
-
   jeep.coords = req.body.coords;
   res.send(jeep);
-
-  /*
-  Sample PUT request
-  {
-    "id": 1,
-    "coords": [
-        14.6575533,
-        121.0742258
-    ]
-  }
-  */
-
 })
-
-
-
-
-
 
 module.exports = router;
 
@@ -111,21 +103,14 @@ module.exports = router;
 // endpoints needed
 
 
-GET all jeeps
-GET a single jeep
-
 PUT a single jeep
   modify values
-
-
-
-
 
 */
 
 
 /*
-
+Sample coordinates
 [14.65945055,    121.0726833]
 [14.64827247,    121.0737752]
 [14.652349354571994, 121.06794118881227 ]
@@ -136,8 +121,6 @@ PUT a single jeep
 
 ===================================================
 editing the coords of a jeep
-
-
 
 UPDATE tracker SET coords = '(coords.x , coords.y)' WHERE id = 1;
 
